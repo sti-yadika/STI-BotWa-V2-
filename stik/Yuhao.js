bd0ad145e8d6aceffb35dd83b97deeb5b202c610
hymannn8124 <117699850+bochilascript@users.noreply.github.com>
2025-03-14 15:29:07 +0700

    Add files via upload

--- stik/Yuhao.js
+++ stik/Yuhao.js
@@ -309,6 +309,62 @@ if (budy.toLowerCase() == jawaban) {
 } else console.log('*Jawaban Salah!*')
 }
 
+zanspiw.autoshalat = zanspiw.autoshalat || {}
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? zanspiw.user.id : m.sender
+let id = m.chat 
+
+if (id in zanspiw.autoshalat) return false
+
+let jadwalSholat = {
+    shubuh: '04:39',
+    terbit: '05:44',
+    dhuha: '06:02',
+    dzuhur: '12:02',
+    ashar: '15:15',
+    magrib: '17:52',
+    isya: '19:01',
+}
+
+const datek = new Date((new Date).toLocaleString("en-US", { timeZone: "Asia/Jakarta" }))
+const hours = datek.getHours().toString().padStart(2, "0")
+const minutes = datek.getMinutes().toString().padStart(2, "0")
+const timeNow = `${hours}:${minutes}`
+
+for (let [sholat, waktu] of Object.entries(jadwalSholat)) {
+    if (timeNow === waktu) {
+        let thumbnail = fs.readFileSync('./baseoka/image/jdw.png') // Load gambar
+
+        zanspiw.autoshalat[id] = {
+            timeout: setTimeout(() => {
+                delete zanspiw.autoshalat[id]
+            }, 57000)
+        }
+
+        zanspiw.sendMessage(m.chat, {
+            audio: { url: 'https://media.vocaroo.com/mp3/1ofLT2YUJAjQ' },
+            mimetype: 'audio/mp4',
+            ptt: true,
+            contextInfo: {
+                externalAdReply: {
+                    showAdAttribution: true,
+                    mediaType: 1,
+                    mediaUrl: '',
+                    title: `Selamat menunaikan Ibadah Sholat ${sholat}`,
+                    body: `🕑 ${waktu}`,
+                    sourceUrl: '',
+                    thumbnail: thumbnail,
+                    renderLargerThumbnail: true
+                }
+            }
+        }, {
+            quoted: m,
+            mentions: participants.map(a => a.id)
+        })
+    }
+}
+
+
+
 //=========================================//
         //Suit PvP
 	    this.suit = this.suit ? this.suit : {}
